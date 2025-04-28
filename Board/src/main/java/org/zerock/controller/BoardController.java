e20a1147693e938936be7058e42ca66bcef64cad
bbolung <shoh2802@gmail.com>
2025-04-28 17:58:32 +0900

    *

--- Board/src/main/java/org/zerock/controller/BoardController.java
+++ Board/src/main/java/org/zerock/controller/BoardController.java
@@ -1,36 +1,62 @@
 package org.zerock.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.zerock.dto.BoardVO;
 import org.zerock.service.BoardService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
 /*
- * web-study-11 (JSP)
- * BoardServlet 요청 -> BoardServelt -> boardListAction 실행하는 코드 구현
+  /board/boardList : 전체 데이터 반환
+  /board/view : 상세 페이지  
  */
 
-//@Controller : Container에 BoardContrller 등록
-//@RequestMapping("/board") : "/board" 요청하면 BoardController 실행
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
+@Log4j
 public class BoardController {
 	
-	@Autowired
-	private BoardService service;
+	private final BoardService boardService;
+	
+	//임시 확인용
+	@GetMapping("/list")
+	public List<BoardVO> List(Model model) {
+		log.info("-----list-----");
+		return boardService.selectListBoard();
+	}
 	
 	@GetMapping("/boardList")
 	public String boardList(Model model) {
 		
-		List<BoardVO> vo = service.boardList();
+		List<BoardVO> list = boardService.selectListBoard();
 		
-		model.addAttribute("boardList", vo);
+		model.addAttribute("boardList", list);
 		
 		return "boardList";
 	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String insertBoard(BoardVO vo) {
+		//log.info(vo); -> vo 값 홗인용
+		boardService.insetBoard(vo);
+		return "redirect:/board/boardList";
+	}
 }
+
--- Board/src/main/webapp/WEB-INF/views/boardList.jsp
+++ Board/src/main/webapp/WEB-INF/views/boardList.jsp
@@ -6,7 +6,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>게시글 리스트</title>
+<title>Insert title here</title>
 <link rel="stylesheet" type="text/css" href="../resources/css/shopping.css">
 </head>
 <body>
@@ -15,7 +15,7 @@
       <table class="list">
          <tr>
             <td colspan="5" style="border: white; text-align: right"><a
-               href="BoardServlet?command=board_write_form">게시글 등록</a></td>
+               href="/board/register">게시글 등록</a></td>
          </tr>
          <tr>
             <th>번호</th>
@@ -24,13 +24,13 @@
             <th>작성일</th>
             <th>조회</th>
          </tr>
-         <c:forEach var="board" items="${boardList}">
+         <c:forEach var="board" items="${boardList }">
             <tr class="record">
                <td>${board.num }</td>
                <td><a href="BoardServlet?command=board_view&num=${board.num}">
-                     ${board.title} </a></td>
+                     ${board.title } </a></td>
                <td>${board.name}</td>
-               <td><fmt:formatDate value="${board.writeDate}" /></td>
+               <td><fmt:formatDate value="${board.writeDate }" /></td>
                <td>${board.readCount}</td>
             </tr>
          </c:forEach>
